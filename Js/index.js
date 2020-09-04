a488e2fc657e44fecc08aca7849679fc25eb5c01
Velo <hartley.max@outlook.com>
2020-09-04 20:00:54 +0100

    win lines added

--- Js/index.js
+++ Js/index.js
@@ -1,6 +1,7 @@
 const tiles = document.querySelectorAll('.tile');
 const victor = document.querySelector('.win-message');
 const playButton = document.querySelector('.win-screen');
+const lines = document.querySelectorAll('.line');
 
 let moveCounter = 0;
 let playState = true;
@@ -41,27 +42,32 @@ const makeMove = () => {
 
 const checkWin = () => {
     // check rows
-    return board[0] && board[0] === board[1] && board[1] === board[2] ? gameOver(board[0]) :
-    board[3] && board[3] === board[4] && board[4] === board[5] ? gameOver(board[3]) :
-    board[6] && board[6] === board[7] && board[7] === board[8] ? gameOver(board[6]) :
+    return board[0] && board[0] === board[1] && board[1] === board[2] ? gameOver(board[0], 0) :
+    board[3] && board[3] === board[4] && board[4] === board[5] ? gameOver(board[3], 1) :
+    board[6] && board[6] === board[7] && board[7] === board[8] ? gameOver(board[6], 2) :
     // check columns
-    board[0] && board[0] === board[3] && board[3] === board[6] ? gameOver(board[0]) : 
-    board[1] && board[1] === board[4] && board[4] === board[7] ? gameOver(board[1]) : 
-    board[2] && board[2] === board[5] && board[5] === board[8] ? gameOver(board[2]) :
+    board[0] && board[0] === board[3] && board[3] === board[6] ? gameOver(board[0], 3) : 
+    board[1] && board[1] === board[4] && board[4] === board[7] ? gameOver(board[1], 4) : 
+    board[2] && board[2] === board[5] && board[5] === board[8] ? gameOver(board[2], 5) :
     // check diagonals
-    board[0] && board[0] === board[4] && board[4] === board[8] ? gameOver(board[0]) : 
-    board[2] && board[2] === board[4] && board[4] === board[6] ? gameOver(board[2]) :
+    board[0] && board[0] === board[4] && board[4] === board[8] ? gameOver(board[0], 7) : 
+    board[2] && board[2] === board[4] && board[4] === board[6] ? gameOver(board[2], 6) :
     true;
 }
 
-const gameOver = (winner) => {
+const gameOver = (winner, line) => {
     playState = false;
-    playButton.classList.add('win-screen-active');
+    if (line < 6) {
+        lines[line].classList.add("line-won");
+    } else {
+        lines[line].classList.add(`line-${line + 1}-won`);
+    }
     if(winner) {
         victor.innerHTML = `${winner} win!`;
     } else {
         victor.innerHTML = "It's a draw!";
     }
+    setTimeout(() => {playButton.classList.add('win-screen-active')}, 1200);
 }
 
 const resetBoard = () => {
@@ -69,6 +75,9 @@ const resetBoard = () => {
     moveCounter = 0;
     playButton.classList.remove('win-screen-active');
     board.fill(false);
+    lines.forEach(line => {
+        line.classList.remove('line-won', "line-7-won", "line-8-won")
+    })
 }
 
 const playAgain = () => {
